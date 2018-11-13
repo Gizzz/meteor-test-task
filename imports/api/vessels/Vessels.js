48632cb304f4d7822055b0ee29ce0e3b68c77445
Bulat Husainov <bulatkhusainov@yandex.ru>
2018-11-13 20:59:28 +0300

    add meteor method stub for getting data from rest endpoint

--- imports/api/vessels/Vessels.js
+++ imports/api/vessels/Vessels.js
@@ -1,3 +1,15 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
 
 export default Vessels = new Mongo.Collection('vessels');
+
+Meteor.methods({
+  'vessels.getLocationData'(mmsi) {
+    check(mmsi, Number);
+
+    console.log(mmsi)
+
+    return 'foobar';
+  },
+});
