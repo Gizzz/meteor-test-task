167185d9427f3d64229c10fce98720d8e200ec5c
Bulat Husainov <bulatkhusainov@yandex.ru>
2018-11-13 20:59:51 +0300

    (in work) call meteor method from client

--- imports/ui/components/Autocomplete.jsx
+++ imports/ui/components/Autocomplete.jsx
@@ -1,4 +1,6 @@
+import { Meteor } from 'meteor/meteor';
 import React from 'react';
+import axios from 'axios';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
@@ -22,10 +24,24 @@ class Autocomplete extends React.Component {
     this.setState({ searchResults });
   }
 
+  create_searchResultClick_handler = (mmsi) => {
+    return () => {
+      // # target server do not support CORS, so need to use methods, maybe
+      //
+      // const url = `https://api.aprs.fi/api/get?name=${mmsi}&what=loc&apikey=109676.BrVSr9i6TpbMbBe&format=json`;
+      // axios.get(url).then(res => console.log(res.data));
+
+      const res = Meteor.call('vessels.getLocationData', mmsi);
+      console.log(res);
+
+      return null;
+    };
+  }
+
   render() {
     const searchResults = this.state.searchResults.map((item, index) => {
       return (
-        <li key={index}>
+        <li key={index} onClick={this.create_searchResultClick_handler(item.MMSI)}>
           {item.Name}
         </li>
       );
@@ -37,7 +53,7 @@ class Autocomplete extends React.Component {
         {' '}
         <Button variant="outlined">Search</Button>
         <h3>Search results:</h3>
-        <ul>
+        <ul className="suggestions">
           {searchResults}
         </ul>
       </div>
