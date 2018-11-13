e9310f93f9160526b6bd47666da4005e99786ab8
Bulat Husainov <bulatkhusainov@yandex.ru>
2018-11-13 19:04:34 +0300

    make autocomplete fetch data from meteor collection

--- imports/ui/components/Autocomplete.jsx
+++ imports/ui/components/Autocomplete.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+import Vessels from '../../api/vessels/Vessels.js';
 
 class Autocomplete extends React.Component {
+  state = {
+    searchTerm: '',
+    searchResults: [],
+  }
+
+  handleChange = (e) => {
+    const term = e.target.value;
+
+    if (term.trim() === '') {
+      this.setState({ searchResults: [] });
+      return;
+    }
+
+    const searchResults = Vessels.find({ Name: { $regex: `${term}`, $options: 'i' } }).fetch();
+    this.setState({ searchResults });
+  }
+
   render() {
+    const searchResults = this.state.searchResults.map((item, index) => {
+      return (
+        <li key={index}>
+          {item.Name}
+        </li>
+      );
+    });
+
     return (
       <div>
-        <TextField type="text" label="Enter a vessel name" />
+        <TextField type="text" label="Enter a vessel name" onChange={this.handleChange} />
+        {' '}
+        <Button variant="outlined">Search</Button>
+        <h3>Search results:</h3>
+        <ul>
+          {searchResults}
+        </ul>
       </div>
     );
   }
