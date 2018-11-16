a03bf207966c1e63bc12aeb0aba79fc24e8845ee
Bulat Khusainov <bulat.khusainov@akvelon.com>
2018-11-16 12:51:00 +0300

    add routing, add about screen

---
+++ imports/ui/AboutScreen/AboutScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutScreen extends React.Component {
+  render() {
+    return (
+      <>
+        <h3>About Screen</h3>
+        <p>Some text</p>
+      </>
+    );
+  }
+}
+
+export default AboutScreen;
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 import HomeScreen from './HomeScreen/HomeScreen.jsx';
+import AboutScreen from './AboutScreen/AboutScreen.jsx';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Meteor Test Task</h1>
-        <HomeScreen />
+      <div className="app">
+        <BrowserRouter>
+          <>
+            <header>
+              <Link to="/">Home</Link>
+              {' | '}
+              <Link to="/about">About</Link>
+              <h1>Meteor Test Task</h1>
+            </header>
+            <Route path="/" exact component={HomeScreen} />
+            <Route path="/about" component={AboutScreen} />
+          </>
+        </BrowserRouter>
       </div>
     );
   }
