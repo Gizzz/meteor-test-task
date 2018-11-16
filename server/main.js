64911c429c40732b5cbd115e09c2a9633f03607f
Bulat Khusainov <bulat.khusainov@akvelon.com>
2018-11-16 14:05:25 +0300

    add validation when seeding Vessels collection

--- server/main.js
+++ server/main.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
 
 import Vessels from '../imports/api/vessels/Vessels.js';
 import vesselsSeedData from '../imports/api/vessels/vesselsSeedData.json';
@@ -6,6 +7,22 @@ import vesselsSeedData from '../imports/api/vessels/vesselsSeedData.json';
 Meteor.startup(() => {
   // If the Vessels collection is empty, add some data.
   if (Vessels.find().count() === 0) {
-    vesselsSeedData.forEach(item => Vessels.insert(item));
+    const schema = new SimpleSchema({
+      Type: { type: String, required: true },
+      Name: { type: String, required: true },
+      IMO: { type: Number, required: true },
+      CS: { type: String, required: true },
+      MMSI: { type: Number, required: true },
+      Length: { type: String, required: true },
+    });
+
+    vesselsSeedData.forEach(item => {
+      const validationContext = schema.newContext();
+      validationContext.validate(item);
+
+      if (validationContext.isValid()) {
+        Vessels.insert(item);
+      }
+    });
   }
 });
