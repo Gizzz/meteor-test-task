991dec84e8b2685e31b49600ff9f011e26886a94
Bulat Khusainov <bulat.khusainov@akvelon.com>
2018-11-22 16:04:38 +0300

    autocomplete: handle keydowns

--- imports/ui/HomeScreen/components/autocomplete/Autocomplete.jsx
+++ imports/ui/HomeScreen/components/autocomplete/Autocomplete.jsx
@@ -11,6 +11,7 @@ class Autocomplete extends React.Component {
   initialState = {
     searchTerm: '',
     suggestions: [],
+    selectedSuggestionIndex: null,
   }
 
   state = this.initialState
@@ -30,38 +31,98 @@ class Autocomplete extends React.Component {
     });
   }
 
+  handleKeydown = (e) => {
+    if (this.state.suggestions.length === 0) { return; }
+
+    if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
+      e.preventDefault();
+
+      const firstSuggestionIndex = 0;
+      const lastSuggestionIndex = this.state.suggestions.length - 1;
+      const selectedSuggestionIndex =
+        e.key === 'ArrowDown'
+          ? this.state.selectedSuggestionIndex === null
+            ? firstSuggestionIndex
+            : this.state.selectedSuggestionIndex < lastSuggestionIndex
+              ? this.state.selectedSuggestionIndex + 1
+              : lastSuggestionIndex
+          : this.state.selectedSuggestionIndex === null
+            ? lastSuggestionIndex
+            : this.state.selectedSuggestionIndex > firstSuggestionIndex
+              ? this.state.selectedSuggestionIndex - 1
+              : firstSuggestionIndex
+
+      this.setState({ selectedSuggestionIndex });
+      return;
+    }
+
+    if (e.key === 'Enter') {
+      const selectedSuggestion = this.state.suggestions[this.state.selectedSuggestionIndex];
+      this.setState({
+        searchTerm: selectedSuggestion.Name,
+        suggestions: [],
+      });
+      this.performLocationSearch(selectedSuggestion.MMSI);
+      return;
+    }
+
+    if (e.key === 'Escape') {
+      this.setState({
+        suggestions: [],
+        selectedSuggestionIndex: null,
+      });
+      return;
+    }
+  }
+
+  handleBlur = () => {
+    this.setState({
+      suggestions: [],
+      selectedSuggestionIndex: null,
+    });
+  }
+
   create_searchResultClick_handler = (mmsi) => {
     return () => {
-      Meteor.call('vessels.getLocationData', mmsi, (err, res) => {
-        if (err) {
-          console.error('Method call finished with error:', err);
-          return;
-        }
-
-        if (!res || !res.data || !res.data.lat || !res.data.lng || !res.data.name) {
-          console.error('Incorrect data shape.');
-          return;
-        }
-
-        const newCoords = {
-          lat: Number(res.data.lat),
-          lng: Number(res.data.lng),
-        };
-
-        this.setState({
-          searchTerm: res.data.name,
-          suggestions: [],
-        });
-
-        this.props.onSearchSuccess(newCoords)
-      });
+      this.performLocationSearch(mmsi);
     };
+  }
+
+  performLocationSearch = (mmsi) => {
+    Meteor.call('vessels.getLocationData', mmsi, (err, res) => {
+      if (err) {
+        console.error('Method call finished with error:', err);
+        return;
+      }
+
+      if (!res || !res.data || !res.data.lat || !res.data.lng || !res.data.name) {
+        console.error('Incorrect data shape.');
+        return;
+      }
+
+      const newCoords = {
+        lat: Number(res.data.lat),
+        lng: Number(res.data.lng),
+      };
+
+      this.setState({
+        searchTerm: res.data.name,
+        suggestions: [],
+      });
+
+      this.props.onSearchSuccess(newCoords)
+    });
   }
 
   render() {
     const suggestions = this.state.suggestions.map((item, index) => {
       return (
-        <MenuItem key={index} selected={false} component="div" onClick={this.create_searchResultClick_handler(item.MMSI)}>
+        <MenuItem
+          key={index}
+          selected={index === this.state.selectedSuggestionIndex}
+          component="div"
+          onClick={this.create_searchResultClick_handler(item.MMSI)}
+        >
           <div>
             <span>
               {item.Name}
@@ -73,7 +134,15 @@ class Autocomplete extends React.Component {
 
     return (
       <div>
-        <TextField type="text" label="Enter a vessel name" fullWidth value={this.state.searchTerm} onChange={this.handleChange} />
+        <TextField
+          type="text"
+          label="Enter a vessel name"
+          fullWidth
+          value={this.state.searchTerm}
+          onChange={this.handleChange}
+          onKeyDown={this.handleKeydown}
+          onBlur={this.handleBlur}
+        />
         <Paper square>
           {suggestions}
         </Paper>
