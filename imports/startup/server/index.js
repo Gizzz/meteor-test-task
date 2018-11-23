7eebf7be1dd4a3415f6345a4c7b34ca0357403a7
Bulat Khusainov <bulat.khusainov@akvelon.com>
2018-11-23 13:56:31 +0300

    meteor: move startup code to /imports/startup

---
+++ imports/startup/server/index.js
@@ -0,0 +1,28 @@
+import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
+
+import Vessels from '../../api/vessels/Vessels.js';
+import vesselsSeedData from '../../api/vessels/vesselsSeedData.json';
+
+Meteor.startup(() => {
+  // If the Vessels collection is empty, add some data.
+  if (Vessels.find().count() === 0) {
+    const schema = new SimpleSchema({
+      Type: { type: String, required: true },
+      Name: { type: String, required: true },
+      IMO: { type: Number, required: true },
+      CS: { type: String, required: true },
+      MMSI: { type: Number, required: true },
+      Length: { type: String, required: true },
+    });
+
+    vesselsSeedData.forEach(item => {
+      const validationContext = schema.newContext();
+      validationContext.validate(item);
+
+      if (validationContext.isValid()) {
+        Vessels.insert(item);
+      }
+    });
+  }
+});
