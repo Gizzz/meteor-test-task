a52caa63bd32001318837a2416aa86d0ce51b542
Bulat Khusainov <bulat.khusainov@akvelon.com>
2018-11-26 13:40:30 +0300

    fix map issue

--- imports/ui/HomeScreen/components/Map.jsx
+++ imports/ui/HomeScreen/components/Map.jsx
@@ -10,7 +10,7 @@ class Map extends React.Component {
       // Important! Always set the container height explicitly
       <div style={{ height: '70vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyAQ4i_puN-HgB8wydbmoC6b9jPp0bo7mUw' }}
+          bootstrapURLKeys={{ key: 'AIzaSyBu1tGP9X-sMDTA7s33bw92PkCBrAMcz7o' }}
           defaultCenter={{ lat: 59.95, lng: 30.33 }}
           center={this.props.coords}
           defaultZoom={11}
